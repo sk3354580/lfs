25889b9612322aa967d80b41ca40b81408d0e600
shiv <70441609+sk3354580@users.noreply.github.com>
2020-10-18 11:48:45 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,96 +1,82 @@
 
-#::: Import modules and packages :::
+
+from __future__ import division, print_function
+# coding=utf-8
+import sys
+import os
+import glob
+import re
+import numpy as np
+
+# Keras
+from tensorflow.keras.applications.imagenet_utils import preprocess_input, decode_predictions
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
+
 # Flask utils
 from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
-from gevent.pywsgi import WSGIServer
+#from gevent.pywsgi import WSGIServer
 
-# Import Keras dependencies
-from tensorflow.keras.models import model_from_json
-from tensorflow.python.framework import ops
-ops.reset_default_graph()
-from keras.preprocessing import image
-
-# Import other dependecies
-import numpy as np
-import h5py
-import os
-
-#::: Flask App Engine :::
-# Define a Flask app
+# Define a flask app
 app = Flask(__name__)
 
-# ::: Prepare Keras Model :::
-# Model files
-MODEL_ARCHITECTURE = 'model_adam.json'
-MODEL_WEIGHTS = 'model_100_eopchs_adam_20190807.h5'
+# Model saved with Keras model.save()
+MODEL_PATH =''
 
-# Load the model from external files
-json_file = open(MODEL_ARCHITECTURE)
-loaded_model_json = json_file.read()
-json_file.close()
-model = model_from_json(loaded_model_json)
-
-# Get weights into the model
-model.load_weights(MODEL_WEIGHTS)
-print('Model loaded. Check http://127.0.0.1:5000/')
-
-
-# ::: MODEL FUNCTIONS :::
+# Load your trained model
+model = load_model(MODEL_PATH)
+model.make_predict_function
 def model_predict(img_path, model):
-	
+    img = image.load_img(img_path, target_size=(224, 224))
 
-	IMG = image.load_img(img_path).convert('L')
-	print(type(IMG))
+    # Preprocessing the image
+    x = image.img_to_array(img)
+    # x = np.true_divide(x, 255)
+    ## Scaling
+    x=x/255
+    x = np.expand_dims(x, axis=0)
+   
 
-	IMG_ = IMG.resize((257, 342))
-	print(type(IMG_))
-	IMG_ = np.asarray(IMG_)
-	print(IMG_.shape)
-	IMG_ = np.true_divide(IMG_, 255)
-	IMG_ = IMG_.reshape(1, 342, 257, 1)
-	print(type(IMG_), IMG_.shape)
+    # Be careful how your trained model deals with the input
+    # otherwise, it won't make correct prediction!
+    x = preprocess_input(x)
 
-	print(model)
+    preds = model.predict(x)
+    preds=np.argmax(preds, axis=1)
+    if preds==0:
+        preds="The Person is Infected With Pneumonia"
+    else:
+        preds="The Person is not Infected With Pneumonia"
+    
+    
+    return preds
 
-	model.compile(loss='categorical_crossentropy', metrics=['accuracy'], optimizer='rmsprop')
-	prediction = model.predict_classes(IMG_)
 
-	return prediction
-
-
-# ::: FLASK ROUTES
 @app.route('/', methods=['GET'])
 def index():
-	# Main Page
-	return render_template('index.html')
+    # Main page
+    return render_template('index.html')
+
 
 @app.route('/predict', methods=['GET', 'POST'])
 def upload():
+    if request.method == 'POST':
+        # Get the file from post request
+        f = request.files['file']
 
-	# Constants:
-	classes = {'TRAIN': ['BACTERIA', 'NORMAL', 'VIRUS'],
-	           'VALIDATION': ['BACTERIA', 'NORMAL'],
-	           'TEST': ['BACTERIA', 'NORMAL', 'VIRUS']}
+        # Save the file to ./uploads
+        basepath = os.path.dirname(__file__)
+        file_path = os.path.join(
+            basepath, 'uploads', secure_filename(f.filename))
+        f.save(file_path)
 
-	if request.method == 'POST':
+        # Make prediction
+        preds = model_predict(file_path, model)
+        result=preds
+        return result
+    return None
 
-		# Get the file from post request
-		f = request.files['file']
-
-		# Save the file to ./uploads
-		basepath = os.path.dirname(__file__)
-		file_path = os.path.join(
-			basepath, 'uploads', secure_filename(f.filename))
-		f.save(file_path)
-
-		# Make a prediction
-		prediction = model_predict(file_path, model)
-
-		predicted_class = classes['TRAIN'][prediction[0]]
-		print('We think that is {}.'.format(predicted_class.lower()))
-
-		return str(predicted_class).lower()
 
 if __name__ == '__main__':
-	app.run(debug = True)
+    app.run(debug=True)
